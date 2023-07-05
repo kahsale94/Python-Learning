d11c92e2d24597aef65ef7d74a566ded40d6fd31
Kaiser <ultimateclash22@gmail.com>
2023-07-04 21:51:22 -0300

    End of the day

--- first-commands.py
+++ first-commands.py
@@ -1,1 +1,34 @@
+print("Hello World!")
+print("I'm a Python programmer!")
 print("I have no Enemies!")
+
+print("\n-Sum-")
+print("7 + 4 =", 7 + 4)
+
+print("\n-Subtraction-")
+print("7 - 4 =", 7 - 4)
+
+print("\n-Multiplication-")
+print("7 * 4 = ", 7 * 4)
+
+print("\n-Division-")
+print("7 / 4 =", 7 / 4)
+
+print("\n-Floor Division-")
+print("7 // 4 =", 7 // 4)
+
+print("\n-Modulus-")
+print("7 % 4 =", 7 % 4)
+
+print("\n-Exponent-")
+print("7 ** 4 = ", 7 ** 4)
+
+name = "Kaiky"
+age = 19
+weight = 79.5
+print(name, age, weight)
+
+name = input("What's your name? ")
+age = input("How old are you? ")
+weight = input("What's your weight? ")
+print("\nName:", name,  "\nAge:", age, "\nWeight:", weight)
